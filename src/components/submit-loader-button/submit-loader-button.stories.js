54306029beaa6552cb3d5e74adf231090904dddd
Eder Díaz <ederchrono@gmail.com>
2019-10-12 20:55:46 -0500

    🐛 add errored and disabled state for submit-loader-button

--- src/components/submit-loader-button/submit-loader-button.stories.js
+++ src/components/submit-loader-button/submit-loader-button.stories.js
@@ -102,3 +102,35 @@ export const errored = () => ({
   },
   template: '<submit-loader-button @click="reset" @ready="startLoading" :error="error" :progress="loadProgress"/>'
 })
+
+
+export const erroredAndDisabled = () => ({
+  components,
+  data () {
+    return {
+      error: true,
+      loadProgress: 0
+    }
+  },
+  methods: {
+    reset () {
+      this.error = false
+    },
+    startLoading () {
+      // simulate loading
+      setTimeout(() => {
+        this.loadProgress = 0.2
+      }, 700)
+      setTimeout(() => {
+        this.loadProgress = 0.4
+      }, 1000)
+      setTimeout(() => {
+        this.loadProgress = 0.7
+      }, 1500)
+      setTimeout(() => {
+        this.loadProgress = 1
+      }, 3000)
+    }
+  },
+  template: '<submit-loader-button disabled @click="reset" @ready="startLoading" :error="error" :progress="loadProgress"/>'
+})
